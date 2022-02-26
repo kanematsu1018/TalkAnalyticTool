057a52b967d5086af674c34618303264adaadb36
taku kanematsu <kanematsu.1018@gmai.com>
2022-02-26 17:14:02 +0900

    日本語フォント探索

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,17 @@ input_data = ""
 is_input_file = False
 
 
+# フォントを全て読み込み
+fonts = set([f.name for f in plt.font_manager.fontManager.ttflist])
+ 
+# 描画領域のサイズ調整
+plt.figure(figsize=(10,len(fonts)/4))
+ 
+# フォントの表示
+for i, font in enumerate(fonts):
+    st.write(plt.text(0, i, f"日本語：{font}", fontname=font))
+
+
 # ラジオボタンの表示
 selected_item = st.radio('音声ファイルの入力形式を指定してください',
                             ['Youtube', 'mp3'])
